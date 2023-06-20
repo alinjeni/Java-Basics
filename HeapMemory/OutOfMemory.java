b2af7d417e2710a9a93c014865d0710bea3ed9a5
Alin Jeni C <90136926+alinjeni@users.noreply.github.com>
2023-06-21 01:07:47 +0530

    Update OutOfMemory.java

--- HeapMemory/OutOfMemory.java
+++ HeapMemory/OutOfMemory.java
@@ -6,6 +6,8 @@ public class OutOfMemory{
             while (true) {
                 String str = new String("string object");
             }
+            //long arraysize=1000000000;
+            //int[] Array1=new int[arraysize];
         } catch (OutOfMemoryError e) {
             System.out.println("Out of memory exception occurred.");
         }
